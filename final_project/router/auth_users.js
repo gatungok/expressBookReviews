ba563d07c6f37760a71805e2c660a6c87f59c691
gatungok <kelvin.gatungo@gmail.com>
2025-06-11 18:42:12 -0400

    Implement session-based authentication with JWT tokens

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,16 +7,39 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+    let userswithsamename = users.filter((user) => {
+        return user.username === username
+    });
+    // Return true if username exists, false otherwise
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+     let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password)
+    });
+    // Return true if username and password match, false otherwise
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+
+// Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in" });
+    }
 });
 
 // Add a book review
