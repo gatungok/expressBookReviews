fa19c66bbfacfa06d68fb27b5f05fe69bef0e7f6
gatungok <kelvin.gatungo@gmail.com>
2025-06-15 21:50:25 -0400

    Updated general user routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,15 +17,31 @@ public_users.get('/', function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if (book) {
+        return res.status(200).json(JSON.stringify(book, null, 2));
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    const booksByAuthor = [];
+    const keys = Object.keys(books);
+    for (const key of keys) {
+        if (books[key].author === author) {
+            booksByAuthor.push(books[key]);
+        }
+    }
+    if (booksByAuthor.length > 0) {
+        return res.status(200).json(JSON.stringify(booksByAuthor, null, 2));
+    } else {
+        return res.status(404).json({ message: "No books found by this author" });
+    }
 });
 
 // Get all books based on title
